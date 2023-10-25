319acf888aeb90f35076b26d63caab85fbdc79c2
Harsh kumar <harshkr5247@gmail.com>
2023-10-25 16:01:21 +0530

    modified homepage

--- client/src/components/DivHavingContentCards/Card.jsx
+++ client/src/components/DivHavingContentCards/Card.jsx
@@ -1,38 +1,19 @@
 import { BiSolidLike } from "react-icons/bi";
-import {useState, useEffect} from "react"
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-const Card = ({ id, title, thumbnail, url, views, likes, cloudinaryID, popularity, vote_count }) => {
-  const [image, setImage] = useState(thumbnail);
-
-  useEffect(() => {
-    if (!thumbnail) {
-      // Make an API call to fetch the image
-      fetch("http://localhost:6000/image", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ movie_id: id }),
-      })
-        .then((response) => {
-          if (response.ok) {
-            return response.text();
-          } else {
-            throw new Error("API error");
-          }
-        })
-        .then((data) => {
-          console.log("Images Api Call: ", data);
-          setImage(data);
-        })
-        .catch((error) => {
-          console.error("Error fetching image:", error);
-        });
-    }
-  }, [id, thumbnail]);
-
+const Card = ({
+  MovieID,
+  MovieName,
+  url,
+  views,
+  likes,
   
+  Popularity,
+  VoteCount,
+  PosterURL,
+}) => {
   
+
   function formatCompactNumber(number) {
     const formatter = Intl.NumberFormat("en", { notation: "compact" });
     return formatter.format(number);
@@ -42,27 +23,29 @@ const Card = ({ id, title, thumbnail, url, views, likes, cloudinaryID, popularit
     backgroundPosition: "center",
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
-    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${thumbnail})`,
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${PosterURL})`,
   };
 
   return (
-    <Link to={{
-      pathname: "/chatroom/" + id,
-      search: title,
-      hash: title,
-      state: { title: "hello" }
-    }}>
+    <Link
+      to={{
+        pathname: "/chatroom/" + MovieID,
+        search: MovieName,
+        hash: MovieName,
+        state: { MovieName: "hello" },
+      }}
+    >
       <div className="card" style={sectionStyle}>
         <div></div>
         <div>
-          <h2>{title}</h2>
+          <h2>{MovieName}</h2>
         </div>
         <div className="card-data">
           <div>
-            <p>{formatCompactNumber(views ? views : vote_count * 4)} views</p>
+            <p>{formatCompactNumber(views ? views : VoteCount * 4)} views</p>
           </div>
           <div className="flex-row">
-            <p>{formatCompactNumber(likes ? likes : vote_count)}</p>{" "}
+            <p>{formatCompactNumber(likes ? likes : VoteCount)}</p>{" "}
             <BiSolidLike
               style={{
                 fontSize: "1.2em",
--- models/CUSTOMAI/ai.py
+++ models/CUSTOMAI/ai.py
@@ -1,15 +1,13 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 import re
-from transformers import T5ForConditionalGeneration, T5Tokenizer
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity, linear_kernel
-from transformers import T5ForConditionalGeneration, T5Tokenizer
-
+import requests
 
 app = Flask(__name__)
 CORS(app)  # Add this line to enable CORS for all routes
+
+api_key = '5b923f780b5401e92b81a39fb35f266d'
 
 
 @app.route("/")
@@ -23,57 +21,60 @@ def recommend_movies():
     user_query = data.get("searchText")
 
     # Your code for extracting genres and generating movie recommendations
-    potential_genres = ['war', 'mystery', 'tv movie', 'crime', 'family', 'western', 'animation', 'action', 'fantasy', 'adventure', 'foreign', 'romance', 'drama', 'comedy', 'documentary', 'thriller', 'horror', 'music', 'science fiction', 'history']
-    genres = [genre for genre in potential_genres if genre.lower() in user_query.lower()]
-    filtered_query = re.sub("|".join(genres), "", user_query, flags=re.IGNORECASE)
+    potential_genres = ['war', 'mystery', 'tv movie', 'crime', 'family', 'western', 'animation', 'action', 'fantasy', 'adventure',
+                        'foreign', 'romance', 'drama', 'comedy', 'documentary', 'thriller', 'horror', 'music', 'science fiction', 'history']
+    genres = [genre for genre in potential_genres if genre.lower()
+              in user_query.lower()]
+    filtered_query = re.sub("|".join(genres), "",
+                            user_query, flags=re.IGNORECASE)
     overview = filtered_query.strip()
-
-    # Initialize the T5 model and tokenizer
-    model = T5ForConditionalGeneration.from_pretrained('t5-small')
-    tokenizer = T5Tokenizer.from_pretrained('t5-small')
-
-    # Generate the output using the T5 model
-    input_text = f"Genres: {', '.join(genres)} Overview: {overview} "
-    inputs = tokenizer.encode(input_text, return_tensors="pt", max_length=512, truncation=True)
-
-    # Generate the output text
-    outputs = model.generate(inputs, max_length=100, num_beams=4, early_stopping=True)
-    output_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-
-    # Define a regular expression pattern to match genres and overview
-    parts = output_text.split("Overview:")
-    genre1 = ""
-    overview1 = ""
-    if len(parts) == 2:
-        genre_part, overview_part = parts
-        genres = [genre.strip() for genre in genre_part.split(",")]
-        genres[0] = genres[0].replace("Genres: ", "")
-        genre = ""
-        for i in genres:
-            genre += i+' '
-        genre1 = genre
-        overview = overview_part.replace("|endoftext|>", "").strip()
-        overview1 = overview
 
     # Load movie data from a CSV file
     movie_data = pd.read_csv("tmdb_5000_movies.csv")
 
     # Filter the dataset based on the input genres
-    input_genres = genre.split()
-    matching_movies = movie_data[movie_data['genres'].str.lower().str.contains('|'.join(input_genres))]
-    matching_movies = matching_movies.sort_values(by='popularity', ascending=False)
+    input_genres = genres
+    matching_movies = movie_data[movie_data['genres'].str.lower(
+    ).str.contains('|'.join(input_genres))]
+    matching_movies = matching_movies.sort_values(
+        by='popularity', ascending=False)
 
     # Get the top 10 popular movies
     top_10_popular_movies = matching_movies.head(6)
 
     # Prepare the response in JSON format
     response_data = {
-        "Genres": genre1,
-        "Overview": overview1,
-        "RecommendedMovies": top_10_popular_movies[['id', 'title', 'popularity', 'vote_count']].to_dict(orient='records')
+        "RecommendedMovies": []
     }
 
+    for index, row in top_10_popular_movies.iterrows():
+        movie_id = row['id']
+        movie_name = row['title']
+        popularity = row['popularity']
+        vote_count = row['vote_count']
+
+        poster_url = get_movie_poster_url(movie_id)
+        movie_info = {
+            "MovieName": movie_name,
+            "MovieID": movie_id,
+            "Popularity": popularity,
+            "VoteCount": vote_count,
+            "PosterURL": poster_url
+        }
+        response_data["RecommendedMovies"].append(movie_info)
+
     return jsonify(response_data)
+
+
+def get_movie_poster_url(movie_id):
+    response = requests.get(
+        f'https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}')
+    if response.status_code == 200:
+        poster = response.json()
+        poster_path = poster.get('poster_path')
+        if poster_path:
+            return 'https://image.tmdb.org/t/p/w500/' + poster_path
+    return None
 
 
 if __name__ == "__main__":
